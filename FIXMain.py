1df728da351d286375d2ab40fb1fa153ce30b18a
silence-vin <393752795@qq.com>
2016-05-15 18:31:27 +0800

    add describe for API keys
    add describe to remind users to add API keys here

--- FIXMain.py
+++ FIXMain.py
@@ -9,8 +9,8 @@ import Application
 
 if __name__ == '__main__':
     config = 'settings.cfg'
-    accesskey = '3346e714-2311-48aa-b4e2-082df25e55ce' #test
-    secretkey = 'ccb9b3d2-6be5-41ae-b9c6-bb84c771e2b4'
+    accesskey = 'add your accesskey here' # add your API keys created in BTCC exchange
+    secretkey = 'add your secretkey here'
 
     settings = fix.SessionSettings (config)
     storeFactory = fix.FileStoreFactory (settings)
